7bfcc4e579a3a4ef368a9c787ce6edae191bf613
Vincent Barilla <vbarilla@gmail.com>
2016-09-23 11:24:12 -0700

    Debugging data transfer between server and client

--- index.js
+++ index.js
@@ -1,29 +1,7 @@
 var express = require('express');
 var app = express();
-var server = require('http').createServer(app);
+var server = require('http').Server(app);
 var io = require('socket.io')(server);
-console.log("Listening on localhost:3000");
-server.listen(3000);
-
-io.on('connection', function(socket){ 
-
-  console.log("SOCKET CONNECTED");
-
-  socket.on('send candidate', function(candidate){
-    console.log('CANDIDATE: ', candidate);
-    console.log('PARSED CANDIDATE DATA: ', JSON.parse(candidate).candidate);
-    io.emit('candidate from server ', candidate);
-  });
-
-  socket.on('send stream', function(stream){
-    console.log('STREAM: ', sream);
-    console.log('PARSED STREAM DATA: ', JSON.parse(stream).sdp);
-    io.emit('stream from server ', stream);
-  });
-
-});
-
-
 
 app.use(express.static('public'));
 
@@ -31,6 +9,31 @@ app.get('/', function(req, res) {
   res.sendFile('index');
 });
 
+io.on('connection', function(socket){ 
+
+  console.log("SOCKET CONNECTED");
+
+  socket.on('send candidate', function(candidate){
+    console.log('CANDIDATE: ', candidate);
+    //console.log('PARSED CANDIDATE DATA: ', JSON.parse(candidate).candidate);
+    io.emit('stream from server', candidate);
+  });
+
+  socket.on('send stream', function(stream){
+    console.log('STREAM: ', stream);
+    //console.log('PARSED STREAM DATA: ', JSON.parse(stream).sdp);
+    io.emit('stream from server', stream);
+  });
+
+  socket.on('disconnect', function() {
+    console.log('a user disconnected.');
+  })
+});
+
+
+server.listen(3000, function() {
+  console.log("Listening on localhost:3000");
+});
 
 //app.listen(3000);
 
